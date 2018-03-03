b5b9459801ca91bd9b69404bc254738fba5a29e0
able <1091423911@qq.com>
2018-03-03 19:20:44 +0800

    Update: 待测试

--- xyyb/index.php
+++ xyyb/index.php
@@ -1,7 +1,7 @@
 <?php
     define("APP_DEBUG", true);
-    //define("CITE_ROOT", "https://duncan.ducnan.cn/");
-    define("CITE_ROOT", "http://localhost/");
+    define("CITE_ROOT", "https://duncan.ducnan.cn/");
+    //define("CITE_ROOT", "http://localhost/");
     define("HUI_CSS_URL", CITE_ROOT . "campus/xyyb/Public/h-ui/css/");
     define("HUI_JS_URL", CITE_ROOT . "campus/xyyb/Public/h-ui/js/");
     define("HUI_IMG_URL", CITE_ROOT . "campus/xyyb/Public/h-ui/images/");
